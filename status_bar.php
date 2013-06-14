fbe1e647723f3b9b38a820bc79f3214afa3c37ba
Daniel Carrero <dcarreroc@gmail.com>
2013-06-14 10:28:24 -0300

    Create status_bar.php

---
+++ status_bar.php
@@ -0,0 +1,7 @@
+<?php if (isset($status_bar)): ?>
+  <div style="background:#FAFAFA;text-align: left; padding-left: 25px;bottom: 0;width: 100%;height: 25px;position: fixed; border-top: 1px solid #CCCCCC;">
+    <?php echo $status_bar; ?>
+  </div>
+<?php endif; ?>
+
+<!-- p4a_footer --> 
